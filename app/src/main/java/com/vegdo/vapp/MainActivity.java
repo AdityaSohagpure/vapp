a7e0d6f8f81d7b7aa78e2cff307de03b496dbc53
AdityaSohagpure <adityamhsohagpure999@gmail.com>
2022-01-24 19:03:51 +0530

    start

---
+++ app/src/main/java/com/vegdo/vapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vegdo.vapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
